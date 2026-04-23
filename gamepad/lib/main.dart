8c75771204816e083a3920a2420bfb47dbaaf10f
gusthavo <gusthavoeduardolima615@gmaail.com>
2026-04-23 01:54:10 -0300

    implement the buttonbox function

--- gamepad/lib/main.dart
+++ gamepad/lib/main.dart
@@ -30,11 +30,38 @@ class Control extends StatelessWidget{
           Row(
             mainAxisAlignment: MainAxisAlignment.center, children: [
               SizedBox(width: 40),
-              
+            
             ],
           )
          ],
       )
     );
   }
+}
+
+class ButtonBox extends StatefulWidget{
+  final String letra;
+  final Color cor;
+  const ButtonBox({super.key, required this.letra, required this.cor});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Buttonbox();
+  }
+}
+class _Buttonbox extends State<ButtonBox> {
+  bool _pressionado = false;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _pressionado = true),
+      onTapUp: (_) => setState(() => _pressionado = false),
+      onTapCancel: () => setState(() => _pressionado = false),
+
+
+    );
+  }
+  
 }
